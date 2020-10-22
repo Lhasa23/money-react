74fd33c5c8d8a728067dd2aa20164836d92b5449
eleven-shi <sydryy@gmail.com>
2020-10-22 22:08:37 +0800

    first amend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
+import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+function App () {
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from="/" to="/money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav />
+            </Wrapper>
+        </Router>
+    );
+}
+
+function Statistics() {
+    return <h2>Home</h2>;
+}
+
+function Tags() {
+    return <h2>About</h2>;
+}
+
+function Money() {
+    return <h2>Users</h2>;
+}
+
+function NoMatch() {
+    return <h2>404</h2>;
 }
 
 export default App;
